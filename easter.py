eded2882f3f28a78f027b8f366f95ffba37442ff
Rud Faden <rudfaden@gmail.com>
2024-06-28 14:10:03 +0200

    fix: return date object and not string

--- easter.py
+++ easter.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 
-
-def easter(date_str, input_format='%Y-%m-%d', output_format='%B %d, %Y'):
+def easter(date_str, input_format='%Y-%m-%d'):
     try:
         date_obj = datetime.strptime(date_str, input_format)
         year = date_obj.year
@@ -10,21 +9,21 @@ def easter(date_str, input_format='%Y-%m-%d', output_format='%B %d, %Y'):
         c = year % 100
         d = b // 4
         e = b % 4
-        f = (b+8) // 25
-        g = (b-f+1) // 3
-        h = (19*a+b-d-g+15) % 30
+        f = (b + 8) // 25
+        g = (b - f + 1) // 3
+        h = (19 * a + b - d - g + 15) % 30
         i = c // 4
         k = c % 4
-        l = (32+2*e+2*i-h-k) % 7
-        m = (a+11*h+22*l) // 451
-        n = (h+l-7*m+114) // 31
-        p = (h+l-7*m+114) % 31
-        easter = datetime(year=year, month=n, day=p+1)
-        formatted_date = easter.strftime(output_format)
-        return formatted_date
+        l = (32 + 2 * e + 2 * i - h - k) % 7
+        m = (a + 11 * h + 22 * l) // 451
+        n = (h + l - 7 * m + 114) // 31
+        p = (h + l - 7 * m + 114) % 31
+        easter_date = datetime(year=year, month=n, day=p + 1)
+        return easter_date
     except ValueError as e:
         return str(e)
 
-
-paaske = easter('2024-06-28',output_format='%Y-%m-%d')
-print(paaske)
+# Example usage
+paaske = easter('2018-06-28')
+print(paaske)  # Output: 2024-03-31 00:00:00
+print(paaske.date())
