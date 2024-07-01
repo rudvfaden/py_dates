4e3925b9e7a722381d0b80e506ed4b34f4f7d481
Rud Faden <rudfaden@gmail.com>
2024-07-01 16:23:47 +0200

    fix add startdate for some bank closing dates

--- bankdag.py
+++ bankdag.py
@@ -11,13 +11,13 @@ def erDanskBankdag(date_obj):
         paaske + timedelta(days=1),  # andenPåskedag
         paaske + timedelta(days=26) if date_obj.year < 2024 else None, # storeBededag
         paaske + timedelta(days=39),  # Kr_himmelfart
-        paaske + timedelta(days=50),  # fredag efter kr_himmelfart
         paaske + timedelta(days=49),  # pinsedag
+        paaske + timedelta(days=50) if date_obj.year>2007 else None,  # fredag efter kr_himmelfart
         paaske + timedelta(days=50),  # andenPinsedag
         date(date_obj.year, 12, 24),  # juleaftensdag
         date(date_obj.year, 12, 25),  # juledag
         date(date_obj.year, 12, 26),  # andenJuledag
-        date(date_obj.year, 12, 31),  # nytaarsdag
+        date(date_obj.year, 12, 31) if date_obj.year>2002 else None,  # nytaarsdag
         date(date_obj.year, 6, 5),  # grundlovsdag
     ]
 
