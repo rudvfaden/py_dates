22dfac81d2738930fa55df72d446d4c4c31a3c3c
Rud Faden <rudfaden@gmail.com>
2025-02-09 16:07:37 +0100

    refactor: simplify easter date calculation logic for improved readability

--- danish_banking_holidays/easter.py
+++ danish_banking_holidays/easter.py
@@ -17,21 +17,23 @@ def easter(year: int) -> date:
     if year < 1583:
         raise ValueError("Year must be greater than 1582")
     try:
-        a = year % 19
-        b = year // 100
-        c = year % 100
-        d = b // 4
-        e = b % 4
-        f = (b + 8) // 25
-        g = (b - f + 1) // 3
-        h = (19 * a + b - d - g + 15) % 30
-        i = c // 4
-        k = c % 4
-        ll = (32 + 2 * e + 2 * i - h - k) % 7
-        m = (a + 11 * h + 22 * ll) // 451
-        n = (h + ll - 7 * m + 114) // 31
-        p = (h + ll - 7 * m + 114) % 31
-        easter_date = date(year=year, month=n, day=p + 1)
+        golden_number = year % 19
+        century = year // 100
+        year_in_century = year % 100
+        century_leap_years = century // 4
+        century_remainder = century % 4
+        secular_correction = (century + 8) // 25
+        lunar_correction = (century - secular_correction + 1) // 3
+        moon_sync = (19 * golden_number + century -
+                     century_leap_years - lunar_correction + 15) % 30
+        century_years_div4 = year_in_century // 4
+        year_remainder = year_in_century % 4
+        day_offset = (32 + 2 * century_remainder + 2 *
+                      century_years_div4 - moon_sync - year_remainder) % 7
+        correction = (golden_number + 11 * moon_sync + 22 * day_offset) // 451
+        month = (moon_sync + day_offset - 7 * correction + 114) // 31
+        day = (moon_sync + day_offset - 7 * correction + 114) % 31
+        easter_date = date(year=year, month=month, day=day + 1)
         return easter_date
     except ValueError as e:
         return str(e)
